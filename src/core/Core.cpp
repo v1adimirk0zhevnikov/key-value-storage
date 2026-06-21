f6da117d816a46560a520acc65cecd5b760a3368
v1adimirk0zhevnikov <valdemar@example.com>
2026-06-21 21:01:53 +0700

    Deleted unused

--- src/core/Core.cpp
+++
@@ -1,6 +0,0 @@
-#include "Core.hpp"
-#include <iostream>
-
-void Core::foo() const {
-	std::cout << "[Core] - foo was called" << std::endl;
-}
--- src/core/Core.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_TEST_HPP
-#define CORE_TEST_HPP
-
-class Core final {
-public:
-	void foo() const;	
-};
-
-#endif
